43fc1fca9f19506be9a83323de280e8665df164c
Alexander Melo <alexandermelox@gmail.com>
2018-10-11 10:38:40 -0400

    created the Login and Register modal

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
+import { openModal } from '../../modals/modalActions';
 const { Item } = Menu;
 
 class NavBar extends Component {
@@ -11,9 +13,11 @@ class NavBar extends Component {
   };
 
   handleSignIn = () => {
-    this.setState({
-      authenticated: true
-    });
+    this.props.openModal('LoginModal');
+  }
+
+  handleRegister = () => {
+    this.props.openModal('RegisterModal');
   }
 
   handleSignOut = () => {
@@ -44,7 +48,7 @@ class NavBar extends Component {
           {authenticated ? (
             <SignedInMenu signOut={this.handleSignOut} />
           ) : (
-            <SignedOutMenu signIn={this.handleSignIn} />
+            <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister} />
           )}
         </Container>
       </Menu>
@@ -52,4 +56,8 @@ class NavBar extends Component {
   }
 }
 
-export default withRouter(NavBar);
+const actions = {
+  openModal
+}
+
+export default withRouter(connect(null, actions)(NavBar));
