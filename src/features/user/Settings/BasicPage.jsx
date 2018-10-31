5ec799c5c0ae0de3ebf59b87f7988136015b51ce
Alexander Melo <alexandermelox@gmail.com>
2018-10-31 14:03:58 -0400

    setup basics page for user profile

--- src/features/user/Settings/BasicPage.jsx
+++ src/features/user/Settings/BasicPage.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Segment, Form, Header, Divider, Button} from 'semantic-ui-react';
+import {Field, reduxForm} from 'redux-form';
+import { DateInput, PlaceInput, TextInput } from '../../../app/common/form';
 
-const BasicPage = () => {
-  return (
-    <div>
-      <h1>Basic Page</h1>
-    </div>
-  );
+class BasicPage extends Component {
+  render() {
+    const {pristine, submitting} = this.props;
+    return (
+      <Segment>
+        <Header dividing size='large' content='Basics' />
+        <Form>
+          <Field
+            width={8}
+            name='displayName'
+            type='text'
+            component={TextInput}
+            placeholder='Known As'
+          />
+          <Form.Group inline>
+            {/* TODO: Gender Radio button */}
+          </Form.Group>
+          <Field
+            width={8}
+            name='dateOfBirth'
+            component={DateInput}
+            placeholder='Date of Birth'
+          />
+          <Field
+            name='city'
+            placeholder='Home Town'
+            options={{types: ['(cities)']}}
+            label='Female'
+            component={PlaceInput}
+            width={8}
+          />
+          <Divider/>
+          <Button disabled={pristine || submitting} size='large' positive content='Update Profile'/>
+        </Form>
+      </Segment>
+    );
+  }
 }
 
-export default BasicPage;
+export default reduxForm({form: 'userProfile', enableReinitialize: true})(BasicPage);
--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -9,13 +9,16 @@ import PhotosPage from './PhotosPage';
 import AccountPage from './AccountPage';
 import { updatePassword } from '../../auth/authActions';
 
-const SettingsDashboard = ({ updatePassword, providerId }) => {
+const SettingsDashboard = ({ updatePassword, providerId, user }) => {
   return (
     <Grid>
       <Grid.Column width={12}>
         <Switch>
           <Redirect exact from='/settings' to='/settings/basic'/>
-          <Route path='/settings/basic' component={BasicPage} />
+          <Route 
+            path='/settings/basic'
+            render={() => <BasicPage initialValues={user} />}
+          />
           <Route path='/settings/about' component={AboutPage} />
           <Route path='/settings/photos' component={PhotosPage} />
           <Route 
@@ -32,7 +35,8 @@ const SettingsDashboard = ({ updatePassword, providerId }) => {
 }
 
 const mapStateToProps = state => ({
-  providerId: state.firebase.auth.providerData[0].providerId
+  providerId: state.firebase.auth.providerData[0].providerId,
+  user: state.firebase.profile
 })
 
 
